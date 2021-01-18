24210f878114102a7c9ae91c675a2f578d6b4537
Carmelo <melonemark8@gmail.com>
2021-01-18 13:02:02 +0100

    application structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { MaterialModule } from './shared/material.module';
 import { NgModule } from '@angular/core';
+import { ContainerComponent } from './core/feature/container/container.component';
+import { ContainerSearchComponent } from './core/feature/container-search/container-search.component';
+import { ContainerWidgetComponent } from './core/feature/container-widget/container-widget.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    ContainerSearchComponent,
+    ContainerWidgetComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/core/feature/container-widget/container-widget.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-container-widget',
+  templateUrl: './container-widget.component.html',
+  styleUrls: ['./container-widget.component.css']
+})
+export class ContainerWidgetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
